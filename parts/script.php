9c2acf1575b4c4be695daf3d1db35d2ce55240d5
Zdendoo <denis.valkovic@student.ukf.sk>
2026-04-05 18:23:44 +0200

    Opravené chyby + další part - Script.php

---
+++ parts/script.php
@@ -0,0 +1,5 @@
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/custom.js"></script>
